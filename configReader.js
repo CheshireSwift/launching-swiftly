479633708ce6fb389c824dedfce24986ed68aba5
Sam Collard <swift@swifteagle.co.uk>
2018-02-06 16:32:37 +0000

    Style cleanup

--- configReader.js
+++ configReader.js
@@ -1,23 +1,23 @@
 'use strict'
-let _ = require('lodash')
+const _ = require('lodash')
 
 function readJSON(json) {
   read(JSON.parse(json))
 }
 function read(config) {
-  let modules = config.modules
-  let templateAreas = _(config.layout)
+  const modules = config.modules
+  const templateAreas = _(config.layout)
     .map(row => `"${row.join(' ')}"`)
     .join('')
 
-  let unusedModules = _.difference(_.keys(config.modules), _.flatten(config.layout))
-  let unusedModuleWarnings = _.map(unusedModules, moduleName => `Module "${moduleName}" is defined but not used.`)
+  const unusedModules = _.difference(_.keys(config.modules), _.flatten(config.layout))
+  const unusedModuleWarnings = _.map(unusedModules, moduleName => `Module "${moduleName}" is defined but not used.`)
 
   // TODO: dedup layout entries
-  let undefinedModules = _.difference(_.flatten(config.layout), _.keys(config.modules))
-  let undefinedModuleWarnings = _.map(undefinedModules, moduleName => `Module "${moduleName}" is not defined.`)
+  const undefinedModules = _.difference(_.flatten(config.layout), _.keys(config.modules))
+  const undefinedModuleWarnings = _.map(undefinedModules, moduleName => `Module "${moduleName}" is not defined.`)
 
-  let warnings = _.flatten([
+  const warnings = _.flatten([
     unusedModuleWarnings,
     undefinedModuleWarnings
   ])
